4b9712058c37bd24b18855385f87652de8487896
徐锦涛 <xujintao@haomo-studio.com>
2019-01-23 14:20:59 +0800

    send--静态页面提交

--- src/components/I18n/zhjs.js
+++ src/components/I18n/zhjs.js
@@ -7,5 +7,11 @@ export  default {
     Tab: {
         helloworld: "哈喽世界",
         send:'发送'
+    },
+    // send 中文
+    deSend: {
+        forminput: "收款人名称",
+        forminput2: "Integral number",
+        forminput3: "Please fill in the notes",
     }
 }
