b4fcf269b5f438219a5b361953bb1c3ae0abdf21
caiyongwei <caiyongwei@haomo-studio.com>
2019-01-24 00:56:26 +0800

    样式优化

--- src/scenes/Exchange/Exchange_rest.js
+++ src/scenes/Exchange/Exchange_rest.js
@@ -25,14 +25,14 @@ export default class ExchangeRest extends Component<Props> {
         var cla=''+info.item.cla;
         var times=''+info.item.times;
         return <TouchableOpacity 
-        style={{flexDirection:'row',height:64,padding:20,backgroundColor:"#ffffff"}}>
-            <View style={{marginRight:20,justifyContent: 'center',alignItems: 'center'}}>
+        style={styles.list}>
+            <View style={styles.imgview}>
                 <Image 
-                style={{width:32,height:32,borderRadius:16}}
+                style={styles.imglist}
                 source={Images.Img.transaction}></Image>
             </View>
             <View style={styles.Col}>
-                <Text style={{fontSize:18,color:'#33383d',lineHeight:19}}>{content}LP</Text>
+                <Text style={styles.contentext}>{content}<Text>LP</Text></Text>
                 <Text>{cla}</Text>
             </View>
             <View style={styles.Col}>
@@ -42,8 +42,9 @@ export default class ExchangeRest extends Component<Props> {
       }
       _sectionComp = (info) => {
         var txt = info.section.key;
-        return <Text
-          style={{ height: 30, textAlign:"left", textAlignVertical: 'center',  color: '#8E959E', fontSize: 16 }}>{txt}</Text>
+        return <View style={{flexDirection:"row",margin:15}}>
+            <Text style={styles.towtext}>{txt}</Text>
+          </View>
       }
     render() {
         var section = [
@@ -57,28 +58,28 @@ export default class ExchangeRest extends Component<Props> {
             <View>
             <ScrollView >
             {/*第一块*/}
-            <View style={{flex:1,padding:20,backgroundColor:'#ffffff',margin:0}}> 
+            <View style={styles.chunk}> 
                 {/*第一行*/}
                 <View style={styles.row}>
-                    <View style={{marginRight:20,justifyContent: 'center',alignItems: 'center'}}>
+                    <View style={styles.imgview}>
                         <Image 
-                        style={{width:54,height:54,borderRadius:27}}
+                        style={styles.chunkimg}
                         source={Images.Img.weiwei}></Image>
                     </View>
                     <View style={styles.Col}>
-                        <View style={{width:103,height:22}}>
-                            <Text style={{fontSize:16,color:'#2f3263',lineHeight:22}}>Roxie Shelton</Text>
+                        <View style={{height:22}}>
+                            <Text style={styles.nametext}>Roxie Shelton</Text>
                         </View>
-                        <View style={{width:135,height:17}}>
-                            <Text style={{fontSize:12,color:'#8e959d',lineHeight:17}}>roxieshelton@gmail.com</Text>
+                        <View style={styles.mail}>
+                            <Text style={styles.mailtext}>roxieshelton@gmail.com</Text>
                         </View>
                     </View>
                     <View style={{textAlign:"rigth",marginTop:37}}>
                         <TouchableOpacity 
-                        style={{width:78,height:30,borderWidth:1,borderColor:"#6ad9e1",borderRadius:3}}
+                        style={styles.butborder}
                         onPress={()=>{Actions.exchange}}
                         >
-                            <Text style={{fontSize:12,lineHeight:30,color:"#6ad9e1",textAlign:"center"}}>EXCHANGE</Text>
+                            <Text style={styles.buttext}>EXCHANGE</Text>
                         </TouchableOpacity>
                         
                     </View>
@@ -88,10 +89,10 @@ export default class ExchangeRest extends Component<Props> {
                     {/* 第一列 */}
                     <View style={styles.Col}>
                         <View>
-                            <Text style={{fontSize:20,color:'#33383d',lineHeight:28}}>2.71</Text>
-                            <View style={{borderWidth:2, borderColor:'#78aef9',width:22}}></View>
+                            <Text style={styles.onetext}>2.71</Text>
+                            <View style={styles.blueline}></View>
                             <View style={{height:19,width:69}}>
-                                <Text style={{fontSize:12,color:'#8e959e',lineHeight:19}}>Today Total</Text>
+                                <Text style={styles.towtext}>Today Total</Text>
                             </View>
                         </View>
                     </View>
@@ -99,10 +100,10 @@ export default class ExchangeRest extends Component<Props> {
                     {/* 第二列 */}
                     <View style={styles.Col}>
                         <View>
-                            <Text style={{fontSize:20,color:'#33383d',lineHeight:28}}>3.40</Text>
-                            <View style={{borderWidth:2, borderColor:'#ffcd47',width:22}}></View>
+                            <Text style={styles.onetext}>3.40</Text>
+                            <View style={styles.yellowline}></View>
                             <View style={{height:19,width:91}}>
-                                <Text style={{fontSize:12,color:'#8e959e',lineHeight:19}}>This Week Total</Text>
+                                <Text style={styles.towtext}>This Week Total</Text>
                             </View>
                         </View>
                     </View>
@@ -110,10 +111,10 @@ export default class ExchangeRest extends Component<Props> {
                     <View style={styles.verline}></View>
                     <View style={styles.Col}>
                         <View>
-                            <Text style={{fontSize:20,color:'#33383d',lineHeight:28}}>6.56</Text>
-                            <View style={{borderWidth:2, borderColor:'#f9504d',width:22}}></View>
+                            <Text style={styles.onetext}>6.56</Text>
+                            <View style={styles.redline}></View>
                             <View style={{height:19,width:97}}>
-                                <Text style={{fontSize:12,color:'#8e959e',lineHeight:19}}>This Month Total</Text>
+                                <Text style={styles.towtext}>This Month Total</Text>
                             </View>
                         </View>
                     </View>
@@ -121,11 +122,12 @@ export default class ExchangeRest extends Component<Props> {
             </View>
             
             {/*第二行*/}            
-            <View style={{padding:20}}>
+            <View>
                 <SectionList
                 renderSectionHeader={this._sectionComp}
                 renderItem={this._renderItem}
                 sections={section}
+                style={styles.sectilist}
                 ItemSeparatorComponent={() => <View><Text></Text></View>}
                 />
             </View>  
@@ -141,13 +143,28 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
+    chunk:{flex:1,padding:20,backgroundColor:'#ffffff',margin:0},
+    imgview:{marginRight:20,justifyContent: 'center',alignItems: 'center'},
+    chunkimg:{width:54,height:54,borderRadius:27},
+    nametext:{fontSize:16,color:'#2f3263',lineHeight:22},
+    mail:{width:135,height:17},
+    mailtext:{fontSize:12,color:'#8e959d',lineHeight:17},
+    butborder:{width:78,height:30,borderWidth:1,borderColor:"#6ad9e1",borderRadius:3},
+    buttext:{fontSize:12,lineHeight:30,color:"#6ad9e1",textAlign:"center"},
     row:{flexDirection:'row',height:100},
-    Col:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'stretch', 
-      },
-    verline:{borderWidth:1, borderColor:'#eeeeee',height:30,margin:30},
+    Col:{flex:1,justifyContent:'center',alignItems:'stretch'},
+    blueline:{borderWidth:2, borderColor:'blue',width:22},
+    yellowline:{borderWidth:2, borderColor:'yellow',width:22},
+    redline:{borderWidth:2, borderColor:'red',width:22},
+    verline:{borderWidth:1, borderColor:'#eeeeee',height:30,marginTop:30,marginRight:15},
+    onetext:{fontSize:20,color:'#33383d',lineHeight:28},
+    towtext:{fontSize:12,color:'#8e959e',lineHeight:19},  
+    list:{flexDirection:'row',height:64,padding:10,backgroundColor:"#ffffff",borderWidth:1, borderColor:'white',borderRadius:5}, 
+    sectilist:{margin:20,}, 
+    keytext:{ height: 30, textAlign:"left", textAlignVertical: 'center',  color: '#8E959E', fontSize: 16 },
+    contentext:{fontSize:18,color:'#33383d',lineHeight:19},
+    imglist:{width:32,height:32,borderRadius:16},
+
     
 
 });
